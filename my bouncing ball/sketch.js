aa56bd6f9d0eb0256f79e09b1be3ef22e7dd5a73
Michael Nielsen <michaelnielsen2248@gmail.com>
2021-09-23 09:58:59 +0200

    bouncing ball

--- my bouncing ball/sketch.js
+++ my bouncing ball/sketch.js
@@ -1,18 +1,41 @@
 
-let xPos = 0;
-let yPos = 200;
-let Size = 75
-let speed = 1
-
+let xPos = 200;
+let yPos = xPos
+let Size = 40
+let xSpeed = 1
+let ySpeed = 2
+let diameter = Size * 2
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
 
-circle(xPos,yPos,Size)
-xPos = xPos +(3*speed);
+  // Tegner bolden
+  circle(xPos, yPos, diameter)
 
+  xPos = xPos +  xSpeed
+  yPos = yPos +  ySpeed
 
+  // Farve til bolden
+  let pastMiddleOfCanvas = width / 2 > xPos
+  if (pastMiddleOfCanvas) {
+    fill('yellow')
+  } else {
+    fill('orange')
+  }
+
+  // For den til at flytte sig fra 
+  let pastLeftEdge = 0 > xPos - Size
+  let pastRightEdge = width < xPos + Size
+  if (pastRightEdge || pastLeftEdge) {
+    xSpeed = -xSpeed
+  }
+
+  let belowbottom = height < yPos + Size
+  let abovetop = 0 > yPos - Size
+  if(belowbottom || abovetop){
+    ySpeed = -ySpeed
+  }
 }
