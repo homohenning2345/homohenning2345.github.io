ac49d426e8cce9658ed5ed7804690926f14652c5
Michael Nielsen <michaelnielsen2248@gmail.com>
2021-11-25 08:23:14 +0100

    kkkkk

--- tegneprogram/sketch.js
+++ tegneprogram/sketch.js
@@ -1,12 +1,18 @@
+// Start points
+let x1 = []
+let y1 = []
+// End points
+let x2 = []
+let y2 = []
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
+
 
 function draw() {
+  background(120)
 
-}
-function mouseDragged()
-{
-  strokeWeight(10);
-  line(mouseX, mouseY, pmouseX, pmouseY)
+
 }
