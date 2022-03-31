49589e1818517641dbad613d23fb3f71b22c62d3
Michael Nielsen <michaelnielsen2248@gmail.com>
2022-03-31 11:58:59 +0200

    Update 1.

--- discord bot/bot.js
+++ discord bot/bot.js
@@ -1,1 +1,23 @@
-console.log("Beep Hey Beep😂😂😊")
+
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+require('dotenv').config()
+
+const TOKEN = process.env.TOKEN
+
+console.log(TOKEN)
+
+client.login(TOKEN);
+
+client.on('ready', readydiscord)
+function readydiscord(){
+console.log('🐉Logged in🐉')
+}
+
+client.on('messageCreate', Hej)
+
+function Hej(msg){
+    console.log(msg)
+}
