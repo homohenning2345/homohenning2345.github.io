9b123556e93d1994375a40b536e4622b3d155bff
Michael Nielsen <michaelnielsen2248@gmail.com>
2022-04-07 10:05:57 +0200

    added more code

--- discord bot/bot.js
+++ discord bot/bot.js
@@ -1,7 +1,7 @@
 
 
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 require('dotenv').config()
 
@@ -12,12 +12,28 @@ console.log(TOKEN)
 client.login(TOKEN);
 
 client.on('ready', readydiscord)
-function readydiscord(){
-console.log('🐉Logged in🐉')
+function readydiscord() {
+    console.log(`Logged in as ${client.user.tag}!`);
 }
 
 client.on('messageCreate', Hej)
 
-function Hej(msg){
-    console.log(msg)
+function Hej(msg) {
+    console.log(msg.author.username)
+    console.log(msg.content)
+
+    const bottestingchannel = process.env.CHANNEL_ID
+
+    const replies = [
+    'Jeg elsker også dig💘',
+    'Jeg elsker dig endnu mere',
+    'Awww hvor er du sød',
+    'Skal vi giftes?'
+    ]
+
+    if (msg.channel.id === bottestingchannel && msg.content === "Jeg elsker dig") {
+        const index = Math.floor(Math.random() * replies.length)
+        const reply = replies[index]
+        msg.channel.send(reply)
+    }
 }
