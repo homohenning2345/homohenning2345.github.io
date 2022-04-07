4dd5c018dcdca1ebdbefbb6e9ca1e8d6e4bec410
Michael Nielsen <michaelnielsen2248@gmail.com>
2022-04-07 10:50:31 +0200

    gif

--- discord bot/bot.js
+++ discord bot/bot.js
@@ -1,4 +1,4 @@
-
+const fetch = require("node-fetch")
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -18,22 +18,34 @@ function readydiscord() {
 
 client.on('messageCreate', Hej)
 
-function Hej(msg) {
+async function Hej(msg) {
     console.log(msg.author.username)
     console.log(msg.content)
 
     const bottestingchannel = process.env.CHANNEL_ID
 
     const replies = [
-    'Jeg elsker også dig💘',
-    'Jeg elsker dig endnu mere',
-    'Awww hvor er du sød',
-    'Skal vi giftes?'
+        'Jeg elsker også dig💘',
+        'Jeg elsker dig endnu mere',
+        'Awww hvor er du sød',
+        'Skal vi giftes?'
     ]
 
-    if (msg.channel.id === bottestingchannel && msg.content === "Jeg elsker dig") {
-        const index = Math.floor(Math.random() * replies.length)
-        const reply = replies[index]
-        msg.channel.send(reply)
+    if (msg.channel.id === bottestingchannel) {
+        if (msg.content === "Jeg elsker dig") {
+
+            const index = Math.floor(Math.random() * replies.length)
+
+            const reply = replies[index]
+            msg.channel.send(reply)
+        }
+        else if (msg.content === "!gif") {
+            msg.channel.send("gif!")
+            const url = `https://g.tenor.com/v1/search?q=cat&key=${process.env.TENORKEY}=&limit=8`
+            const result = await fetch(url)
+            const json = await result.json()
+            console.log(json)
+            msg.channel.send(json.results[0].url)
+        }
     }
 }
