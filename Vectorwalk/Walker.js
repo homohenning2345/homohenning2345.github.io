44afc7a11bd9d318a9f8b101429bc344fb81765e
Michael Nielsen <michaelnielsen2248@gmail.com>
2022-05-12 00:32:39 +0200

    fkefefke

--- Vectorwalk/Walker.js
+++ Vectorwalk/Walker.js
@@ -1,17 +1,16 @@
 class Walker {
-    constructor(x, y) {
-      this.pos = createVector(x, y);
-    }
-  
-    update() {
-      this.pos.x = this.pos.x + random(-1, 1);
-      this.pos.y = this.pos.y + random(-1, 1);
-    }
-  
-    show() {
-      stroke(255, 100);
-      strokeWeight(2);
-      point(this.pos.x, this.pos.y);
-    }
+  constructor(x, y) {
+    this.pos = createVector(x, y);
   }
-  
+
+  update() {
+    this.pos.x = this.pos.x + random(-1, 1);
+    this.pos.y = this.pos.y + random(-1, 1);
+  }
+
+  show() {
+    stroke(0, 255, 0);
+    strokeWeight(25);
+    point(this.pos.x, this.pos.y);
+  }
+}
